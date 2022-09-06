bba310d8071463a08065d07242faab4f3352ede3
79875 <pipochinka@edu.hse.ru>
2022-09-06 19:29:04 +0300

    Task2(refactoring)

--- src/main/java/task2/ArrayUtil.java
+++ src/main/java/task2/ArrayUtil.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ArrayUtil {
-    public void sumOfValue(int[] arr, int value){
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i + 1; j < arr.length; j++){
+    public void sumOfValue(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == value) {
                     System.out.println("[" + arr[i] + ", " + arr[j] + "]");
                     return;
@@ -18,13 +18,37 @@ public class ArrayUtil {
         System.out.println("Подходящих пар нет");
     }
 
-    public void sumOfValueWithStream(Integer[] arr, int value){
-        List<Integer> newArr = Arrays.stream(arr)
-                .filter(p -> Arrays.stream(arr)
-                        .anyMatch(v -> v == value - p ))
-                .limit(1)
+    public void sumOfValueWithStream(int[] arr, int value) {
+
+        Integer[] arr1 = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr1[i] = (Integer) arr[i];
+        }
+        List<Integer> newArr = Arrays.stream(arr1)
+                .filter(p -> Arrays.stream(arr1)
+                        .anyMatch(v -> v == value - p))
+                .limit(2)
                 .collect(Collectors.toList());
-        newArr.add(value - newArr.get(0));
-        System.out.println(newArr);
+        if (newArr.size() < 1){
+            System.out.println("Подходящих пар нет");
+        }else if(newArr.get(0) * 2 == value){
+            int count = 0;
+            for (int i: arr){
+                if (i == newArr.get(0)){
+                    count++;
+                }
+            }
+            if (count == 2){
+                newArr.remove(1);
+            }else {
+                newArr.remove(0);
+            }
+            newArr.add(value -newArr.get(0));
+            System.out.println(newArr);
+        }else {
+            newArr.remove(1);
+            newArr.add(value -newArr.get(0));
+            System.out.println(newArr);
+        }
     }
 }
--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -2,12 +2,12 @@ package task2;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[] {3, 4, 2, 7};
-        Integer[] arr1 = new Integer[]{3, 4, 5, 2, 7, 5};
+        int[] arr = new int[] {5, 3, 4, 2, 7};
+
 
         ArrayUtil arrayUtil = new ArrayUtil();
 
         arrayUtil.sumOfValue(arr, 10);
-        arrayUtil.sumOfValueWithStream(arr1, 10);
+        arrayUtil.sumOfValueWithStream(arr, 10);
     }
 }
